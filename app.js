d19732835fcc45f3b4970ecbe04f20816acea5ee
Wichy007 <123163046+Wichy007@users.noreply.github.com>
2023-03-13 13:59:41 +0700

    add single quote

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-
 function dayByNumber(dayNumber){
-switch (dayNumber){
-    case 0 :
-    console.log(sunday)
-    break;
-    case 1 :
-    console.log(monday)
-    break;
-    case 2 :
-    console.log(tuesday)
-    break;
-    case 3 :
-    console.log(wensday)
-    break;
-    case 4 :
-    console.log(thursday)
-    break;
-    case 5 :
-    console.log(friday)
-    break;
-    case 6 :
-    console.log(saturday)
-    break;
-    default :
-    console.log("invalid day number")
-    break;
+    switch (dayNumber){
+        case 0 :
+        console.log('sunday')
+        break;
+        case 1 :
+        console.log('monday')
+        break;
+        case 2 :
+        console.log('tuesday')
+        break;
+        case 3 :
+        console.log('wensday')
+        break;
+        case 4 :
+        console.log('thursday')
+        break;
+        case 5 :
+        console.log('friday')
+        break;
+        case 6 :
+        console.log('saturday')
+        break;
+        default :
+        console.log("invalid day number")
+        break;
+        }
     }
-}
-
-dayByNumber(0)
+    
+    dayByNumber(0)
